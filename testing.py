226bd1f3c10143539ed066cb665bf0df92a7fb60
Eric Morris <emorris2@outlook.com>
2022-04-15 00:29:35 -0400

    Added main function to test output script

--- testing.py
+++ testing.py
@@ -2,17 +2,19 @@ import wmi
 from pprint import pprint
 import json
 
-# ensure open hardware monitor is installed
+def main():
+    w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+    temperature_infos = w.Sensor()
+    values = { 'temp': {}, 'power': {}, 'fan': {} }
+    for sensor in temperature_infos:
+        if sensor.SensorType==u'Temperature':
+            values['temp'][sensor.Name] = sensor.Value
+        if sensor.SensorType==u'Power':
+            values['power'][sensor.Name] = sensor.Value
+        if sensor.SensorType==u'Fan':
+            values['fan'][sensor.Name] = sensor.Value
 
-w = wmi.WMI(namespace="root\OpenHardwareMonitor")
-temperature_infos = w.Sensor()
-values = { 'temp': {}, 'power': {}, 'fan': {} }
-for sensor in temperature_infos:
-    if sensor.SensorType==u'Temperature':
-        values['temp'][sensor.Name] = sensor.Value
-    if sensor.SensorType==u'Power':
-        values['power'][sensor.Name] = sensor.Value
-    if sensor.SensorType==u'Fan':
-        values['fan'][sensor.Name] = sensor.Value
+    pprint(values)
 
-pprint(values)
+if __name__ == "__main__":
+    main()
